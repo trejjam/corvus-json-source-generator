8098774503178b003a4ea0b8b5090d04a9b73a65
Jan Trejbal <jan@trejbal.land>
2024-08-17 12:47:14 +0200

    Add CorvusSourceGeneratorTests

---
+++ Corvus.Json.SourceGenerator.Tests/CorvusSourceGeneratorTests.EmptyWorks#JsonSchemaTypeGeneratorAttribute.g.verified.cs
@@ -0,0 +1,24 @@
+﻿//HintName: JsonSchemaTypeGeneratorAttribute.g.cs
+using System;
+
+namespace Corvus.Json;
+
+[AttributeUsage(AttributeTargets.Struct, Inherited = false, AllowMultiple = false)]
+internal sealed class JsonSchemaTypeGeneratorAttribute : Attribute
+{
+    public JsonSchemaTypeGeneratorAttribute(string location, bool rebaseToRootPath = false)
+    {
+        this.Location = location;
+        this.RebaseToRootPath = rebaseToRootPath;
+    }
+
+    /// <summary>
+    /// Gets the location for the JSON schema.
+    /// </summary>
+    public string Location { get; }
+
+    /// <summary>
+    /// Gets a value indicating whether to rebase to the root path.
+    /// </summary>
+    public bool RebaseToRootPath { get; }
+}
---
+++ Corvus.Json.SourceGenerator.Tests/CorvusSourceGeneratorTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Corvus.Json.SourceGenerator.Tests;
+
+public class CorvusSourceGeneratorTests
+{
+    [Fact]
+    public Task EmptyWorks() => TestHelper.Verify<CorvusSourceGenerator>(string.Empty);
+
+    [Fact]
+    public Task SimpleWorks() => TestHelper.Verify<CorvusSourceGenerator>(
+        """
+         
+        
+        """
+    );
+}
